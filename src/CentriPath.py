8e8b2d1189c878cf57d53d740a4822e8d86af257
zackoturbo <danielzack@gmx.de>
2023-03-19 15:45:28 +0100

    initialized CentriPath class!

---
+++ src/CentriPath.py
@@ -0,0 +1,22 @@
+
+
+
+class CentriPath:
+    def __init__(self, diam_outlet, width_outlet, diam_hub, len_axial=None, diam_suction=None):
+        self.D_2 = diam_outlet
+        self.b_2 = width_outlet
+        self.D_hub = diam_hub
+        self.len_ax = len_axial if len_axial != None else 0.2*self.D_2
+        self.D_S = diam_suction
+    
+    
+    
+    
+    def plotPath():
+        pass
+    
+    
+
+
+        
+    
