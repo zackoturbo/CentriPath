a16735e53a61833e359b016b90023f51b74c6a5a
zackoturbo <danielzack@gmx.de>
2023-04-30 11:46:17 +0200

    added plot function for inlet and outlet only

--- src/CentriPath.py
+++ src/CentriPath.py
@@ -1,8 +1,8 @@
-
+import matplotlib.pyplot as plt
 
 
 class CentriPath:
-    def __init__(self, diam_outlet, width_outlet, diam_hub, len_axial=None, diam_suction=None):
+    def __init__(self, diam_outlet, width_outlet, diam_hub, diam_suction, len_axial=None):
         self.D_2 = diam_outlet
         self.b_2 = width_outlet
         self.D_hub = diam_hub
@@ -12,11 +12,24 @@ class CentriPath:
     
     
     
-    def plotPath():
-        pass
+    def plot_inlet_outlet(self):
+        plt.title(f"Meriodnal Path (D_2 = {self.D_2})")
+        # PLOT AXIS 
+        plt.hlines(0, -50, 50, linestyles="dotted")
+        plt.vlines(0, 0, 0.5*self.D_2*1.15, linestyles="dotted")
+        
+        # INLET
+        plt.plot([-self.len_ax, -self.len_ax], [0.5*self.D_hub, 0.5*self.D_S], color="black")
+        
+        # OUTELT
+        plt.plot([-0.5*self.b_2, 0.5*self.b_2], [0.5*self.D_2, 0.5*self.D_2], color="black")
+    
+        plt.show()
     
     
 
+test_plot = CentriPath(250, 17, 60, 160, 50)
+test_plot.plot_inlet_outlet()
 
         
     
